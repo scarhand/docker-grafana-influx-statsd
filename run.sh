b140c13727a8e9796d3f63fda4f53e3fe8a95417
Niels van der Zanden <niels@phusion.nl>
2016-06-17 10:07:54 +0200

    Forward ports in run script

--- run.sh
+++ run.sh
@@ -7,4 +7,6 @@ $BASEDIR/stop.sh
 docker run -d --name $CONTAINER_NAME \
     -v $BASEDIR/data:/data \
     -v $BASEDIR/aux:/aux \
+    -p 3000:3000 \
+    -p 8125:8125 \
     scarhand/$CONTAINER_NAME
